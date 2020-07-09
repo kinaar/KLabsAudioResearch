8212ff015815b9a66757aa1e014fe5eeee129ca2
Guerric <guerric.chab@gmail.com>
2020-07-09 19:02:50 +0200

    MainScene created

---
+++ Unity/KLabsAudioTools/Assets/Scripts/Audio/GlobalAudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class GlobalAudioManager : MonoBehaviour
+
+{
+
+    public AudioMixer audioMixer;
+    [Range(0.0f, 1.0f)]
+    public float masterVolume = 0.5f;
+    // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
