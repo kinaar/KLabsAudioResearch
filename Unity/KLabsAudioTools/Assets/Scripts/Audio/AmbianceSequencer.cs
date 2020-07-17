7ef1874cb9c2ddc8a6f36109349990af5b934b79
guerric <guerric.chab@gmail.com>
2020-07-17 12:27:41 +0200

    Ambiance Script Updated

--- Unity/KLabsAudioTools/Assets/Scripts/Audio/AmbianceSequencer.cs
+++ Unity/KLabsAudioTools/Assets/Scripts/Audio/AmbianceSequencer.cs
@@ -2,28 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
-
 public class AmbianceSequencer : MonoBehaviour
 {
 
-    public AudioSource ambianceSource;
-    public AudioClip ambianceLoop;
-    public List<AudioClip> randomSounds;
+    AudioSource ambianceBedSource;
+    AudioSource randomSoundsSource;
+    public AudioClip ambianceBed;
+    public AudioClip[] randomSounds;
     
+    public float fadeTime = 1.0f;
+
     [Range(0, 100)]
     [SerializeField]
     private int panRandomization;
+    private float chronometer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         
+        //ambianceBedSource.Play();
+        //ambianceBedSource.loop = true;
+        ambianceBedSource = gameObject.AddComponent<AudioSource>();
+        randomSoundsSource = gameObject.AddComponent<AudioSource>();
+
+        ambianceBedSource.clip = ambianceBed;
+        ambianceBedSource.playOnAwake = false;
+        ambianceBedSource.loop = true;
+        ambianceBedSource.Play();
+
+        randomSoundsSource.playOnAwake = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        chronometer += Time.deltaTime;
+        //randomSoundsSource.clip = randomSounds[Random.Range(0, randomSounds.Length)];
+        //ambianceSource.PlayOneShot(randomSounds);
         
     }
 }
