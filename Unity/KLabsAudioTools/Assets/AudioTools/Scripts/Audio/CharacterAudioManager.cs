d7927f43a9a63a529b877059b16468ab5a3128ae
guerric <guerric.chab@gmail.com>
2020-07-27 15:08:06 +0200

    Character Updated and Working

--- Unity/KLabsAudioTools/Assets/AudioTools/Scripts/Audio/CharacterAudioManager.cs
+++ Unity/KLabsAudioTools/Assets/AudioTools/Scripts/Audio/CharacterAudioManager.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEditor;
 
 public class CharacterAudioManager : MonoBehaviour
 {
     private AudioSource[] audioSources = new AudioSource[10];
+    public AudioMixerGroup m_audioMixer;
 
     [System.Serializable]
     public class CharaAudioClips
     {
         public enum playOn { fire1, fire2, spacebar, custom }
         public playOn _playOnTrigger;
-        public AudioClip soundToTrig;
+        public AudioClip[] soundToTrig;
         public string customKey;
+
+        [Range (-48.0f, 3.0f)]
+        public float _volume;
+
+        [Range (0.0f, 100.0f)]
+        public float _randomPitch;
+
+        public bool bypassReverb = true;
     }
     public CharaAudioClips[] charaAudioClipsArray;
 
@@ -24,7 +34,8 @@ public class CharacterAudioManager : MonoBehaviour
             GameObject child = new GameObject("Player");
             child.transform.parent = gameObject.transform;
             audioSources[i] = child.AddComponent<AudioSource>();
-            audioSources[i].clip = charaAudioClipsArray[i].soundToTrig;
+            audioSources[i].clip = charaAudioClipsArray[i].soundToTrig[0];
+            audioSources[i].outputAudioMixerGroup = m_audioMixer;
         }
     }
 
@@ -37,32 +48,53 @@ public class CharacterAudioManager : MonoBehaviour
             {
                 if (charaAudioClipsArray[i]._playOnTrigger == CharaAudioClips.playOn.fire1)
                 {
-                    audioSources[i].PlayOneShot(charaAudioClipsArray[i].soundToTrig);
-                    Debug.Log("trig");
+                    playSound(i);
                 }
             }
         }
 
         if (Input.GetMouseButtonDown(1))
+        {
             for (int i = 0; i < charaAudioClipsArray.Length; i++)
             {
                 if (charaAudioClipsArray[i]._playOnTrigger == CharaAudioClips.playOn.fire2)
                 {
-                    audioSources[i].PlayOneShot(charaAudioClipsArray[i].soundToTrig);
-                    Debug.Log("trig");
-                }
-            }
-
-        if (Input.GetKeyDown(charaAudioClipsArray[0].customKey))
-        {
-            for (int i = 0; i < charaAudioClipsArray.Length; i++)
-            {
-                if (charaAudioClipsArray[i]._playOnTrigger == CharaAudioClips.playOn.custom)
-                {
-                    audioSources[i].PlayOneShot(charaAudioClipsArray[i].soundToTrig);
-                    Debug.Log("trig");
+                    playSound(i);
                 }
             }
         }
+
+        for (int i = 0; i < charaAudioClipsArray.Length; i++)
+        {
+            if (charaAudioClipsArray[i].customKey != "")
+            {
+                if (charaAudioClipsArray[i]._playOnTrigger == CharaAudioClips.playOn.custom)
+                {
+                    if (Input.GetKeyDown(charaAudioClipsArray[i].customKey))
+                    {
+                        playSound(i);
+                    }
+                }
+            }
+
+            if (charaAudioClipsArray[i]._playOnTrigger == CharaAudioClips.playOn.spacebar)
+            {
+                if (Input.GetKeyDown("space"))
+                {
+                    playSound(i);
+                }
+            }
+        }
+    }
+
+    void playSound(int i)
+    {
+        float pitchRand = Random.Range(-1.0f, 1.0f);
+        int audioRand = Random.Range(0, charaAudioClipsArray[i].soundToTrig.Length);
+        audioSources[i].pitch = 1 + (pitchRand * (charaAudioClipsArray[i]._randomPitch / 100.0f));
+        audioSources[i].PlayOneShot(charaAudioClipsArray[i].soundToTrig[audioRand]);
+        audioSources[i].bypassReverbZones = charaAudioClipsArray[i].bypassReverb;
+        audioSources[i].volume = Mathf.Pow(10, charaAudioClipsArray[i]._volume / 20.0f);
+        Debug.Log("trig");
     }
 }
